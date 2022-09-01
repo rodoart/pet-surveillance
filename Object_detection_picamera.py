ac66a45e4e0d89c86396d9a673aac7ba4ac35fdf
asdrubal-152 <37197740+asdrubal152@users.noreply.github.com>
2022-09-01 18:48:24 -0500

    Changes to run detection

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -22,8 +22,6 @@
 import os
 import cv2
 import numpy as np
-from picamera.array import PiRGBArray
-from picamera import PiCamera
 import tensorflow as tf
 import argparse
 import sys
@@ -112,67 +110,10 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Initialize camera and perform object detection.
 # The camera has to be set up and used differently depending on if it's a
-# Picamera or USB webcam.
-
-# I know this is ugly, but I basically copy+pasted the code for the object
-# detection loop twice, and made one work for Picamera and the other work
-# for USB.
-
-### Picamera ###
-if camera_type == 'picamera':
-    # Initialize Picamera and grab reference to the raw capture
-    camera = PiCamera()
-    camera.resolution = (IM_WIDTH,IM_HEIGHT)
-    camera.framerate = 10
-    rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
-    rawCapture.truncate(0)
-
-    for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-
-        t1 = cv2.getTickCount()
-        
-        # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
-        # i.e. a single-column array, where each item in the column has the pixel RGB value
-        frame = np.copy(frame1.array)
-        frame.setflags(write=1)
-        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_expanded = np.expand_dims(frame_rgb, axis=0)
-
-        # Perform the actual detection by running the model with the image as input
-        (boxes, scores, classes, num) = sess.run(
-            [detection_boxes, detection_scores, detection_classes, num_detections],
-            feed_dict={image_tensor: frame_expanded})
-
-        # Draw the results of the detection (aka 'visulaize the results')
-        vis_util.visualize_boxes_and_labels_on_image_array(
-            frame,
-            np.squeeze(boxes),
-            np.squeeze(classes).astype(np.int32),
-            np.squeeze(scores),
-            category_index,
-            use_normalized_coordinates=True,
-            line_thickness=8,
-            min_score_thresh=0.40)
-
-        cv2.putText(frame,"FPS: {0:.2f}".format(frame_rate_calc),(30,50),font,1,(255,255,0),2,cv2.LINE_AA)
-
-        # All the results have been drawn on the frame, so it's time to display it.
-        cv2.imshow('Object detector', frame)
-
-        t2 = cv2.getTickCount()
-        time1 = (t2-t1)/freq
-        frame_rate_calc = 1/time1
-
-        # Press 'q' to quit
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-        rawCapture.truncate(0)
-
-    camera.close()
+# USB webcam.
 
 ### USB webcam ###
-elif camera_type == 'usb':
+if camera_type == 'usb':
     # Initialize USB webcam feed
     camera = cv2.VideoCapture(0)
     ret = camera.set(3,IM_WIDTH)
